e8913147886e498d878136a8cd8ebaacca96e49b
Christopher Aronson <christopheraronson@icloud.com>
2019-07-29 15:55:21 -0700

    Initial Commit

---
+++ InsomniChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InsomniChat
+//
+//  Created by Christopher Aronson on 7/29/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InsomniChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InsomniChat
+//
+//  Created by Christopher Aronson on 7/29/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
